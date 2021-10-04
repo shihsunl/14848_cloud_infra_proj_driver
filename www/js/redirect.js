105de51c760d1939123b0981ec42d03e14399bdc
bensonlin321 <bensonlin321@gmail.com>
2021-10-04 00:11:59 -0700

    fix: update reverse proxy

--- www/js/redirect.js
+++ www/js/redirect.js
@@ -1,6 +1,6 @@
 
 function redirect(path, port) {
-    var current_host = window.location.host;
+    var current_host = window.location.hostname;
     var redirect_url = "http://"+current_host+"/"+path+":"+port
     console.log(redirect_url);
     window.location.href = redirect_url;
