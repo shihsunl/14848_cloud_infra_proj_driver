219406c498044f3b053e1c992fc6afed66147d9f
bensonlin321 <bensonlin321@gmail.com>
2021-10-04 11:18:59 -0700

    fix: wrong redirected path

--- www/js/redirect.js
+++ www/js/redirect.js
@@ -1,7 +1,7 @@
 
 function redirect(path, port) {
     var current_host = window.location.hostname;
-    var redirect_url = "http://"+current_host+"/"+path+":"+port
+    var redirect_url = "http://"+current_host + ":" + port + "/" + path
     console.log(redirect_url);
     window.location.href = redirect_url;
     //window.location.replace(redirect_url)
